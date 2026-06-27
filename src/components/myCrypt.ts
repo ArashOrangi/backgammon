056ef39ad48da9b36a3cda6f4ad6c58107f34ad6
ArashOrangi <orangi.arash.ao@gmail.com>
2026-06-27 20:31:37 +0330

    change argon2

--- src/components/myCrypt.ts
+++ src/components/myCrypt.ts
@@ -1,18 +1,34 @@
-import { hash, verify } from "argon2";
+// import { hash, verify } from "argon2";
+
+// const myCrypt = {
+// 	verify: async ({ password, hash }: { password: string; hash: string; }) => {
+// 		const v = await verify(hash, password);
+// 		// console.log({ v, hash, password });
+// 		return v;
+
+// 	},
+// 	hashed: async ({ password }: { password: string; }) => {
+// 		const h = await hash(password);
+// 		// console.log({ h, password });
+// 		return h;
+
+// 	},
+// };
+
+// export default myCrypt;
+
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 12;
 
 const myCrypt = {
-	verify: async ({ password, hash }: { password: string; hash: string; }) => {
-		const v = await verify(hash, password);
-		// console.log({ v, hash, password });
-		return v;
+  verify: async ({ password, hash }: { password: string; hash: string }) => {
+    return bcrypt.compare(password, hash);
+  },
 
-	},
-	hashed: async ({ password }: { password: string; }) => {
-		const h = await hash(password);
-		// console.log({ h, password });
-		return h;
-
-	},
+  hashed: async ({ password }: { password: string }) => {
+    return bcrypt.hash(password, SALT_ROUNDS);
+  },
 };
 
 export default myCrypt;
